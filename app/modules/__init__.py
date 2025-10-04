6c097ec61d81cab13714ebc58c1008018b4a40c0
axel-douglas <axeldouglas98@gmail.com>
2025-10-04 11:53:29 -0300

    Add Mars control logistics module and simulation helpers (#339)

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -25,7 +25,7 @@ from .io import (
     invalidate_targets_cache,
     invalidate_all_io_caches,
 )
-from . import mission_overview
+from . import mission_overview, mars_control
 from .utils import safe_int
 
 __all__ = [
@@ -55,6 +55,7 @@ __all__ = [
     "get_train_and_save",
     # Overview helpers
     "mission_overview",
+    "mars_control",
     # Utils
     "safe_int",
 ]
